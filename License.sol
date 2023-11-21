9a16a8ca806b1bb72e33fa1e8d1611a719564ffb
DiannaPuhrman86 <101540819+DiannaPuhrman86@users.noreply.github.com>
2023-11-21 07:47:35 +0100

    Update License.sol

--- License.sol
+++ License.sol
@@ -1,1 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract ContentLicense is ERC721, Ownable {
+    uint256 public nextTokenId;
+    mapping(uint256 => string) public tokenURIs;
+
+    event LicenseMinted(address indexed owner, uint256 indexed tokenId, string tokenURI);
+
+    constructor(string memory _name, string memory _symbol) ERC721(_name, _symbol) {}
+
+}
